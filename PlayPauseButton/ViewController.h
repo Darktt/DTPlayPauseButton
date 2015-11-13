cbf1cad8a4208ce438371651baa95cfb9dc9d17e
Eden Li <eden@realtime.tw>
2015-11-13 11:59:59 +0800

    Initial Commit

---
+++ PlayPauseButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayPauseButton
+//
+//  Created by EdenLi on 2015/11/13.
+//  Copyright © 2015年 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayPauseButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PlayPauseButton
+//
+//  Created by EdenLi on 2015/11/13.
+//  Copyright © 2015年 Darktt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
